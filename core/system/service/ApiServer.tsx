6834ebe5a9288bf94f0f365bf6608239c551cfe8
FloreDenis2001 <floredenis907@yahoo.com>
2025-04-29 15:31:27 +0300

    up

--- core/system/service/ApiServer.tsx
+++ core/system/service/ApiServer.tsx
@@ -1,12 +1,22 @@
 import HttpResponse from "../httpResponse/HttpResponse";
 
 class ApiServer {
-  api<U, T>(path: string, method: string, body: U, token?: string): Promise<HttpResponse<T>> {
-    const url = `http://${window.location.hostname}:8080/stock-manager/api${path}`;
+  api<U, T>(
+    path: string,
+    method: string,
+    body: U,
+    token?: string
+  ): Promise<HttpResponse<T>> {
+    const protocol = window.location.protocol;
+    const isLocalhost = window.location.hostname === "localhost";
+    const port = isLocalhost ? ":8080" : "";
+    const url = `${protocol}//${window.location.hostname}${port}/stock-manager/api${path}`;
 
     const isFormData = body instanceof FormData;
     const headers: HeadersInit = {
-      ...(isFormData ? {} : { "Content-Type": "application/json;charset=utf-8" }),
+      ...(isFormData
+        ? {}
+        : { "Content-Type": "application/json;charset=utf-8" }),
     };
 
     if (token) {
@@ -16,7 +26,7 @@ class ApiServer {
     const options: RequestInit = {
       method,
       headers,
-      body: isFormData ? body : (body == null ? null : JSON.stringify(body)),
+      body: isFormData ? body : body == null ? null : JSON.stringify(body),
     };
 
     return fetch(url, options);
